d4658d2e34eab40101449cd0a7d684165c54eed9
Wtaru-Nakata <wataru9871@gmail.com>
2021-11-08 16:28:29 +0900

    fixed bug

--- retrieve_novels.py
+++ retrieve_novels.py
@@ -10,17 +10,17 @@ import tqdm
 
 def retrieve_text(ncode,pbar):
     # 全部分数を取得
-    while True:
-        try:
-            info_url = "https://ncode.syosetu.com/novelview/infotop/ncode/{}/".format(ncode)
-            info_res = request.urlopen(info_url)
-            soup = BeautifulSoup(info_res, "html.parser")
-            pre_info = soup.select_one("#pre_info").text
-            num_parts = int(re.search(r"全([0-9]+)部分", pre_info).group(1))
-            break
-        except:
-            pbar.set_description('Error occured while requesting', url)
-            time.sleep(1)
+    try:
+        info_url = "https://ncode.syosetu.com/novelview/infotop/ncode/{}/".format(ncode)
+        info_res = request.urlopen(info_url)
+        soup = BeautifulSoup(info_res, "html.parser")
+        pre_info = soup.select_one("#pre_info").text
+        num_parts = int(re.search(r"全([0-9]+)部分", pre_info).group(1))
+        time.sleep(1)
+    except:
+        pbar.set_description('Error occured while requesting', info_url)
+        raise ValueError
+    honbun = ''
 
     for part in range(1, num_parts+1):
         # 作品本文ページのURL
@@ -36,12 +36,13 @@ def retrieve_text(ncode,pbar):
         soup = BeautifulSoup(res, "html.parser")
 
         # CSSセレクタで本文を指定
-        honbun = soup.select_one("#novel_honbun").text
-        honbun += "\n"  # 次の部分との間は念のため改行しておく
+        honbun_part = soup.select_one("#novel_honbun").text
+        honbun_part += "\n"  # 次の部分との間は念のため改行しておく
         
         pbar.set_description("part {:d} downloaded (total: {:d} parts)".format(part, num_parts))  # 進捗を表示
 
         time.sleep(1)  # 次の部分取得までは1秒間の時間を空ける
+        honbun = honbun + honbun_part
     return honbun
 
 if __name__=="__main__":
@@ -54,6 +55,11 @@ if __name__=="__main__":
     df = df[df['global_point'] > args.min_point]
     pbar = tqdm.tqdm(df.iterrows())
     for idx, row in pbar:
-        honbun = retrieve_text(row['ncode'].lower(),pbar)
+        if os.path.exists(os.path.join('data',row['ncode']+'.txt')):
+            continue
+        try:
+            honbun = retrieve_text(row['ncode'].lower(),pbar)
+        except:
+            continue
         with open(os.path.join('data',row['ncode']+'.txt'), mode='w') as f:
             f.write(honbun)
